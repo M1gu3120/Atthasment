c293d1b27075e859095f8c09132d6d96b0185304
M1gu3120 <miguel93angel@hotmail.com>
2023-03-07 00:06:57 -0500

    Attachmen

---
+++ terpel_attachment.bat
@@ -0,0 +1,353 @@
+@echo off
+color C          
+echo.
+echo.
+echo.             
+echo.               .*(####(/,                                                                                                
+echo.           .##################(                                                                                          
+echo.        ##############/***//(###/                                                                                    
+echo.      ###########(************///#,                                                                                      
+echo.    .##########,       ,********///#         #####                                                             #####      
+echo.    ##########.          .*******///*        #########.    /####(,    #### (##(   /(#######*        ,(####/    #####    
+echo.   .#########(            ,*******//*        ########   /##########   #########  #############    #####%####(   #####    
+echo.   .##########            ,******///*        #####     #####  .####   #####      #####   .####/  ####   (###(  #####     
+echo.    ##########.          .*******///*        #####     ##########*    ####(      #####    ####/ *#########(    #####     
+echo.    .##########,       *********///#         %####.     #####          ####(      #####   #####. .####(         #####     
+echo.      ###########(************///#,          #######,  *##########    ####(      ############    ##########/   *######## 
+echo.        ##############/**///(###*                                                #####                                   
+echo.          .##################(                                                   #####                                   
+echo.              .*/(##((/,                                                         #####                                   
+echo.                                                                                                                                                                   
+echo.   
+echo. [34m                                                  +oooo+                                      [0m                                          
+echo. [34m                                                  ydddds                                     [0m       
+echo. [34m       `````    ``````````````             ```````ydddds        `````         ````````````          
+echo. [34m      `yyyyy-   +yyyyyyyyyyyyso/.      ./osyyyyyyyhdddds    `:osyyyyy.   `-+ssyyyyyyyyyyyy-[0m         
+echo. [34m      `ddddd:   sdddddddddddddddh:    +hddddddddddddddds   -ydddddddd.  .sdddddddddddddddd- [0m        
+echo. [34m      `ddddd:   sddddh////+ohddddy   -dddddy++////hdddds   oddddds+//`  odddddo+////+ddddd-[0m         
+echo. [34m      `ddddd:   sddddy      oddddh`  +ddddd.      ydddds   yddddy       hdddds      `ddddd-[0m         
+echo. [34m      `ddddd:   sddddy      +ddddd`  +ddddh`      ydddds   ydddds       hdddd+      `ddddd-[0m         
+echo. [34m      `ddddd:   sddddy      +ddddd`  /ddddd.      ydddds   ydddds       hdddds`     `ddddd:[0m         
+echo. [34m      `ddddd:   sddddy      +ddddd`  -dddddyoooooohdddds   ydddds       odddddsooooooddddd: [0m        
+echo. [34m      `ddddd:   sddddy      /ddddd`   /hddddddddddddddds   ydddds       `sdddddddddddddddd: [0m        
+echo. [34m      `sssss-   /sssso      :sssss`    `:+ossssssssssss+   +ssss+         ./+ossssssssssss- [0m        
+                                                                                                 
+                                                                                                 
+echo. [34m       ```````````````````````````````````````````````````````````````````````````````````[0m           
+echo. [34m      `:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::-------:::::::`[0m          
+                      
+echo. [34m                                                                               /s  [0m                  
+echo. [34m             .:///:`.////-   `////     :.:///-    `:///:.    .////.   `////    /y/// [0m                
+echo. [34m           .y/...+ys-..-s+   ..-h`    sy:..-o+  `y/...:y.  /s-..-s/   ..-h`   /s...  [0m               
+echo. [34m           /s    `h:    -h     `h`    s/    -h  `y+-.` .`  y:    :y     .h`   /s      [0m              
+echo. [34m           /s    `h:    .h     `h`    s/    -h   `-:/+o+`  y:    :y     .h`   /s       [0m             
+echo. [34m           +s    `h:    .h     `h`    s/    -h  .:    `y:  y/    /y     .h`   /s       [0m             
+echo. [34m           +s    `h:    .y  `-::h:-`  s/    .h  `s+/::+s.  :s/::/yy   -:/h::` .y/-:-    [0m            
+echo. [34m           ..     -`    `-   ------`                                                                               
+echo. 
+echo.                                                                                 
+echo. Terpel a tu servicio IT atajos tecnologicos 
+                                                                                                                           
+pause>nul
+cls
+tittle Bienvenidos a Acceso Directos 
+:dashboard
+cls
+color B0
+pause
+cls
+echo FECHA - %DATE%
+echo HORA  - %TIME%.
+echo.
+echo.
+echo           Atajos rapidos al sistemas 
+echo.                                                                                                                         
+echo          1-  serial del equipo                                        24- clic staff
+echo          2-  ISL online cliente                                       25- clic terpel
+echo          3-  Terpel remoto ISL                                        26- intranet
+echo          4-  informaciÛn del sistema Windows y de tu ordenador       27- aris                                  
+echo          5-  Impresoras Terpel                                        28- asistente
+echo          6-  Acceso office365.com                                     29- Delfos
+echo          7-  borrar temporales                                        30- simad
+echo          8-  Abrir Conexiones de Red                                  31- copiar vistas de sap   
+echo          9-  Administracion de equipos                                
+echo          10- Administrador de tareas
+echo          11- ConfiguraciÛn del sistema
+echo          12- Monitor de rendimiento
+echo          13- Opciones de internet
+echo          14- centro de redes y recursos compartidos 
+echo          15- Usuarios y grupos locales
+echo          16- Visor de eventos
+echo          17- carpeta Instaladores Terpel Indra sw basico 
+echo          18- carpeta Instaladores Terpel Indra sw extra
+echo          19- carpeta Instaladores Terpel Indra
+echo          20- Reset Configuracion Outlook
+echo          21- Saber * Ip - Mac - Dns*  del Equipo
+echo          22- Version BIOS
+echo          23- Agregar y quitar programas                                                                                                          
+    
+
+echo elija una opcion
+
+ 
+set /p dOpt=
+if %dOpt% ==1 goto dOption1
+if %dOpt% ==2 goto dOption2
+if %dOpt% ==3 goto dOption3
+if %dOpt% ==4 goto dOption4
+if %dOpt% ==5 goto dOption5
+if %dOpt% ==6 goto dOption6
+if %dOpt% ==7 goto dOption7
+if %dOpt% ==8 goto dOption8
+if %dOpt% ==9 goto dOption9
+if %dOpt% ==10 goto dOption10
+if %dOpt% ==11 goto dOption11
+if %dOpt% ==12 goto dOption12
+if %dOpt% ==13 goto dOption13
+if %dOpt% ==14 goto dOption14
+if %dOpt% ==15 goto dOption15
+if %dOpt% ==16 goto dOption16
+if %dOpt% ==17 goto dOption17
+if %dOpt% ==18 goto dOption18
+if %dOpt% ==19 goto dOption19
+if %dOpt% ==20 goto dOption20
+if %dOpt% ==21 goto dOption21
+if %dOpt% ==22 goto dOption22
+if %dOpt% ==23 goto dOption23
+if %dOpt% ==24 goto dOption24
+if %dOpt% ==25 goto dOption25
+if %dOpt% ==26 goto dOption26
+if %dOpt% ==27 goto dOption27
+if %dOpt% ==28 goto dOption28
+if %dOpt% ==29 goto dOption29
+if %dOpt% ==30 goto dOption30
+if %dOpt% ==31 goto dOption31
+:dOption1
+start cmd /k wmic csproduct get identifyingnumber
+cls
+goto dashboard
+:dOption2
+start https://www.islonline.com/es/es/join/
+cls
+goto dashboard
+:dOption3
+start http://10.220.0.75/users/main/join.html
+cls
+goto dashboard
+:dOption4
+start MSINFO32
+winver
+cls
+goto dashboard
+:dOption5
+tittle Bienvenidos a Acceso Directos 
+:printerMenu
+cls
+color B0
+pause
+cls
+echo FECHA - %DATE%
+echo HORA  - %TIME%.
+echo.
+echo.
+echo           Impresoras Terpel
+echo.                                                                                                                         
+echo          1- DIR. GRAL  
+echo          2- PLANTA CHIMITA
+echo          3- PLANTA LA MARIA	 
+echo          4- PLANTA MANIZALESr                                 
+echo          5- PLANTA LUBRICANTES
+echo          6- PLANTA NEIVA
+echo          7- OFICINA REG. OCCIDENTE
+echo          8- OFICINA REG. NORTE   
+echo          9- MENU PRINCIPAL  
+
+set /p pOpt=
+if %pOpt% ==1 goto pOption1
+if %pOpt% ==2 goto pOption2
+if %pOpt% ==3 goto pOption3
+if %pOpt% ==4 goto pOption4
+if %pOpt% ==5 goto pOption5
+if %pOpt% ==6 goto pOption6
+if %pOpt%== 7 goto pOption7
+if %pOpt% ==8 goto pOption8
+if %pOpt% ==9 goto pOption9
+
+cls
+color 0c
+cls
+          REM OPTIONAL PRINTER SCREEN
+          :pOption1
+          start \\COLDIRSRVIMP03
+          cls
+          goto printerMenu
+          :pOption2
+          start \\COLBUCSRVIMP03
+          cls
+          goto printerMenu
+          :pOption3
+          start \\COLANTSRVIMP02
+          cls
+          goto printerMenu
+          :pOption4
+          start \\COLCENSRVIMP01
+          cls
+          goto printerMenu
+          :pOption5
+          start \\COLCLUSRVIMP01
+          cls
+          goto printerMenu
+          :pOption6
+          start \\COLSURSRVIMP01
+          cls
+          goto printerMenu
+          :pOption7
+          start \\COLCALSRVIMP02
+          cls
+          goto printerMenu
+          :pOption8
+          start \\COLNORSRVIMP02
+          cls
+          goto printerMenu
+          
+          :pOption9
+          goto dashboard
+:dOption6
+start https://login.microsoftonline.com/common/oauth2/authorize?client_id=00000006-0000-0ff1-ce00-000000000000&response_mode=form_post&response_type=code+id_token&scope=openid+profile&state=OpenIdConnect.AuthenticationProperties%3dU1YpV5azpbheEL7dwWOLaMfmSE8Ik3tC3HyFnln-TFzzavgswZBtqw7xhUCi3GadacUtDiKM46AiKylkypD_GkQ1WCClO9cuKQEMm4kkdpKGyCV9JDU0XTVqo0W4cS_-8Z7qJAzCYCoW9qnR2InooxCdZVTwrMJV-zDf4h5CdUs&nonce=636846813159061541.NTNjM2U0MWUtYjJjOS00NDY4LWFjYWItMmY1N2Q1MTdkYTFjOWNiNDM0OTMtMzUzYS00NzdhLTg3YzAtMWIyZDNkZGRjYjFi&redirect_uri=https%3a%2f%2fportal.office.com%2flanding&ui_locales=es-ES&mkt=es-ES&client-request-id=1cf538d2-1686-4f88-a8b7-1dfb0b4b1aeb&sso_reload=true#/homepage=
+cls
+goto dashboard
+:dOption7
+start del C:\Users\%username%\AppData\Local\Temp /f /s /q
+rd C:\Users\%username%\AppData\Local\Temp /s /q
+del c:\Windows\Temp /f /s /q
+rd c:\Windows\Temp /s /q
+cls
+goto dashboard
+:dOption8
+start RunDll32.exe shell32.dll,Control_RunDLL ncpa.cpl
+goto dashboard
+cls
+goto dashboard
+:dOption9
+start compmgmt.msc
+goto dashboard
+cls
+goto dashboard
+:dOption10
+start taskmgr
+goto dashboard
+cls
+goto dashboard
+:dOption11
+start      msconfig
+goto dashboard
+cls
+goto dashboard
+:dOption12
+start perfmon.msc
+goto dashboard
+cls
+goto dashboard
+:dOption13
+start inetcpl.cpl
+goto dashboard
+cls
+goto dashboard
+:dOption14
+start control /name Microsoft.NetworkAndSharingCenter
+goto dashboard
+cls
+goto dashboard
+:dOption15
+start lusrmgr.msc
+goto dashboard
+cls
+goto dashboard
+:dOption16
+start eventvwr.msc
+goto dashboard
+cls
+goto dashboard
+:dOption17
+start \\COLDIRSRVBK\instaladores\TERPEL-INDRA\Sw-básico
+goto dashboard
+cls
+goto dashboard
+:dOption18
+start \\COLDIRSRVBK\instaladores\TERPEL INDRA\SW extra
+goto dashboard
+cls
+:dOption19
+start \\COLDIRSRVBK\instaladores
+goto dashboard
+cls
+:dOption20
+start  Outlook.exe /resetnavpane 
+goto dashboard
+cls
+:dOption21
+start  cmd /k ipconfig /all
+goto dashboard
+cls
+:dOption22
+start /Output:BIOS.txt  BIOS get /all /format:LIST
+:dOption23
+start appwiz.cpl
+goto dashboard
+:dOption24
+start https://clic.terpel.com/sde/ clic 
+goto dashboard
+cls
+:opcion25
+start https://clic.terpel.com/helpdesk/newlogin.asp
+goto dashboard
+cls}
+:dOption26
+start http://intranet/
+goto dashboard
+cls
+:dOption27
+start http://10.220.0.215:19990/businesspublisher/login.do?login=anonymous&password=anonymous
+goto dashboard
+:opcion28
+start http://colbogsrvbz01/Terpel/
+goto dashboard
+:dOption29
+start http://10.250.0.68/qlikview/login.htm
+goto dashboard
+:dOption30
+start https://simad.terpel.com/backend.php/security/login
+goto dashboard
+PAUSE>NUL
+goto dashboard
+cls
+:dOption31
+start
+:bucle
+if exist "\\COLDIRSRVBK\instaladores\TERPEL INDRA\Common\Common\saplogon.ini" goto :copiar
+if exist "\\COLDIRSRVBK\instaladores\TERPEL INDRA\Common\Common\SapLogonTree.xml" goto :copiar
+if exist "\\COLDIRSRVBK\instaladores\TERPEL INDRA\Common\Common\saprules.xml" goto :copiar
+if exist "\\COLDIRSRVBK\instaladores\TERPEL INDRA\Common\Common\sapshortcut.ini" goto :copiar
+if exist "\\COLDIRSRVBK\instaladores\TERPEL INDRA\Common\Common\SAPUILandscape.xml" goto :copiar
+if exist "\\COLDIRSRVBK\instaladores\TERPEL INDRA\Common\Common\SAPUILandscapeGlobal.xml" goto :copiar
+goto :bucle
+:copiar
+copy "\\COLDIRSRVBK\instaladores\TERPEL INDRA\Common\Common\saplogon.ini" "C:\Users\%username%\AppData\Roaming\SAP\Common"
+copy "\\COLDIRSRVBK\instaladores\TERPEL INDRA\Common\Common\SapLogonTree.xml" "C:\Users\%username%\AppData\Roaming\SAP\Common"
+copy "\\COLDIRSRVBK\instaladores\TERPEL INDRA\Common\Common\saprules.xml" "C:\Users\%username%\AppData\Roaming\SAP\Common"
+copy "\\COLDIRSRVBK\instaladores\TERPEL INDRA\Common\Common\sapshortcut.ini" "C:\Users\%username%\AppData\Roaming\SAP\Common"
+copy "\\COLDIRSRVBK\instaladores\TERPEL INDRA\Common\Common\SAPUILandscape.xml" "C:\Users\%username%\AppData\Roaming\SAP\Common"
+copy "\\COLDIRSRVBK\instaladores\TERPEL INDRA\Common\Common\SAPUILandscapeGlobal.xml" "C:\Users\%username%\AppData\Roaming\SAP\Common"
+echo.
+echo PRESIONE ENTER PARA REGRESAR AL MENU
+pause >nul
+ 
+goto dashboard
+exit                                                                                                                        
+                                                                                                                                   
+pause.
+goto :loop
+
+                                                                                                                                                                                                                                               
+                                                                                                                        
+                                                                                                                                                                                                                           
